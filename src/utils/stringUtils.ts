d5fd637df647b9b82ed83db396bd854f9a2f87ad
GreyStinger <jaydenandrews010@gmail.com>
2023-03-20 15:40:40 +0200

    stringUtils doc

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,7 +1,19 @@
+/**
+ *
+ * Sanitizes the given string by removing all non-alphanumeric characters,
+ * replacing spaces with dashes and trimming any leading/trailing spaces.
+ *
+ * @param str - The string to be sanitized
+ * @returns The sanitized string with only alphanumeric characters, dashes,
+ *  periods, underscores, commas, and spaces.
+ */
 function sanitizeString(str: string): string {
-    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim, "").trim();
+    // Remove all non-alphanumeric characters except spaces, periods,
+    // underscores, commas, and dashes
+    str = str.replace(/[^a-z0-9áéíóúñü .,_-]/gim, "").trim();
+
+    // Replace spaces with dashes
     str = str.replace(/ /g, "-");
+
     return str;
 }
-
-export { sanitizeString };
